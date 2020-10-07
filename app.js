9ded59c497bdef50f6253bade2de78a260fde25e
1mpactBot <64764777+1mpactBot@users.noreply.github.com>
2020-10-07 18:34:28 +0530

    Port updated

--- app.js
+++ app.js
@@ -127,6 +127,8 @@ const List=mongoose.model("List", listSchema);
 
 
 
-app.listen(3000 || process.env.PORT, function() {
-  console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
